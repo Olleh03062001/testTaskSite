b55b818b0051343991bf7f528d86b4976ef8ac3f
Oleh <Olleh03062001@gmail.com>
2020-02-18 20:19:24 +0200

    design updated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log( "work" );
+//при нажатии на миниатюру
+$('#thumb img').click(function(){
+  //удаляем класс active с другой миниатюры
+  $('#thumb img').removeClass("active");
+  //добавляем класс active к миниатюре на которую мы нажали
+  $(this).addClass("active");
+  //получаем у неё значение атрибута src и присваиваем его атрибуту src с id="preview"
+  $("#preview").attr("src",$(this).attr("src"));
+});
--- js/plugins.js
+++ js/plugins.js
@@ -1,3 +1,18 @@
+
+
+//при нажатии на миниатюру
+$('#thumb img').click(function(){
+  //удаляем класс active с другой миниатюры
+  $('#thumb img').removeClass("active");
+  //добавляем класс active к миниатюре на которую мы нажали
+  $(this).addClass("active");
+  //получаем у неё значение атрибута src и присваиваем его атрибуту src с id="preview"
+  $("#preview").attr("src",$(this).attr("src"));
+});
+
+
+
+/*
 // Avoid `console` errors in browsers that lack a console.
 (function() {
   var method;
@@ -22,3 +37,4 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+*/
